9434feba2fe6e0817176197720bc63eb657263f3
Dharm Patel <dharmpatel311287@gmail.com>
2025-12-02 16:20:12 +0530

    Done Practice Object Oriented Programming Project 02

---
+++ Object Oriented Programming/02_Class_Method_Self.py
@@ -0,0 +1,10 @@
+class Car:
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
+
+my_car = Car("AMG", "SLS", "2020")
+print(my_car.model)
+print(my_car.model)
+print(my_car.year)
